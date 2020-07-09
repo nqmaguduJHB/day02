f2ac7900735f7e580d6b2d4c9d98b23b00991a79
nqobile juwao magudu <nqmagudu@c4r11s10.wethinkcode.co.za>
2020-07-09 15:42:23 +0200

    Day02 exercise 05

---
+++ ex05/ft_print_comb2.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nqmagudu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/09 15:20:39 by nqmagudu          #+#    #+#             */
+/*   Updated: 2020/07/09 15:40:42 by nqmagudu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb2(void)
+{
+	char a;
+	char b;
+	char c;
+	char d;
+
+    a = '0';
+	while(a <= '9')
+	{
+		b = '0';
+		while(b <= '8')
+		{
+			c = '0';
+			while(c <= '9')
+			{
+				d = '1';
+				while(d <= '9')
+				{
+					ft_putchar(a);
+					ft_putchar(b);
+					ft_putchar(' ');
+					ft_putchar(c);
+					ft_putchar(d);
+					ft_putchar(',');
+					ft_putchar(' ');
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
